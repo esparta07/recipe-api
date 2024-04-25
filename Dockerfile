7e20b80ba0587c077f1c405f60584553f8cea099
adi <adityachaudhary700@gmail.com>
2024-04-25 14:56:27 +0545

    github waitfor_db

--- Dockerfile
+++ Dockerfile
@@ -28,6 +28,4 @@ RUN python -m venv /py && \
 
 ENV PATH="/py/bin:$PATH"
 
-
 USER django-user
-
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,28 +1,27 @@
 """
-Django command to wait for the database to be avaialable. 
+Django command to wait for the database to be available.
 """
-
-from django.core.management.base import BaseCommand
 import time
 
-from psycopg2 import OperationalError as Pyscopg2OpError
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db import connections
 from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     """Django command to wait for database."""
-    
+
     def handle(self, *args, **options):
-        """ Entrypoint for command."""
-        self.stdout.write('Waiting for database..')
-        db_up = False
-        while db_up is False:
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+        while not db_conn:
             try:
-                self.check(databases=['default'])
-                db_up = True
-            except (Pyscopg2OpError, OperationalError):
-                self.stdout.write('Database unavailabale, waiting 1 second')
+                db_conn = connections['default']
+            except (OperationalError, Psycopg2OpError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-                
-        self.stdout.write(self.style.SUCCESS('DATABASE AVAILABLE!'))
-                
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
